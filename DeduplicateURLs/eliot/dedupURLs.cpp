07b8edae2f607c4145ec960917d8f1307eaa14f8
eflan <eflan@users.noreply.github.com>
2015-01-08 14:01:49 -0800

    Create dedupURLs.cpp
    Initial thoughts on a solution.

---
+++ DeduplicateURLs/eliot/dedupURLs.cpp
@@ -0,0 +1,14 @@
+/**
+for(url in URLs)
+{
+    k = html[url]
+    list = duplicates[k]
+    duplicates[h] = cons(url, list)
+}
+
+for(k in duplicates.keys)
+{
+    list = duplicates[k]
+    print(k, list)
+}
+*/
