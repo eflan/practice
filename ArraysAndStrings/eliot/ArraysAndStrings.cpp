3b107164126e5ec97931e00b32074a4d09276069
Eliot Flannery <eliotf@gmail.com>
2015-01-31 16:19:17 -0800

    Encode spaces solution

--- ArraysAndStrings/eliot/ArraysAndStrings.cpp
+++ ArraysAndStrings/eliot/ArraysAndStrings.cpp
@@ -160,12 +160,97 @@ bool IsPermutation(char *str1, char *str2)
 	return totalChars == 0;
 }
 
+//
+// Example
+// Input: “Mr John Smith    “, 13
+// Output: “Mr%20John%20Smith”
+//
+
+void EncodeSpaces(char *str, size_t length)
+{
+	char *end = (str + length) - 1;
+	char *bufferEnd = str + length;
+
+	if(str == nullptr || length == 0)
+	{
+		return;
+	}
+
+	// Find the end of the buffer of trailing whitespace
+	// Assume it exactly fits the expanded space characters
+	while(*bufferEnd != '\0')
+	{
+		bufferEnd++;
+	}
+	
+	bool munching  = false;
+	size_t countToMove = 0;
+
+	while(end >= str)
+	{
+		// Starting at the end of the string, walk toward the start until we encounter a space
+		if(*end == ' ')
+		{
+			if(!munching)
+			{
+				munching = true;
+				
+				if(reinterpret_cast<size_t>(bufferEnd) < countToMove || 
+				   bufferEnd - countToMove < end)
+				{
+					throw "Buffer overrun! Buffer is too small to expand all spaces!";
+				}
+				
+				// memmove the characters so far to the end of the string
+				char *move = end + 1;
+				memmove(bufferEnd - countToMove, move, countToMove);
+				bufferEnd -= countToMove;
+				countToMove = 0;
+			}
+
+			// Ensure we aren’t overrunning the buffer
+			if(reinterpret_cast<size_t>(bufferEnd) < 3 || bufferEnd - 3 < end)
+			{
+				throw "Buffer too small to expand all spaces!";
+			}
+
+			// munch spaces, encoding them as %20 at the end of the string
+			char *begin = bufferEnd - 3;
+			begin[0] = '%';
+			begin[1] = '2';
+			begin[2] = '0';
+			
+			// move one step toward the start of the string
+			end--;
+			// advance bufferEnd to account for the %20
+			bufferEnd -= 3;
+		}
+		else
+		{
+			if(munching)
+			{
+				munching = false;
+			}		
+			
+			// accumulate another character to move and move end one
+			// step toward the start of the string
+			countToMove++;
+			end--;
+		}
+	}
+	
+	if(countToMove > 0 && bufferEnd != end + 1 + countToMove)
+	{
+		throw "String contained too much trailing whitespace!"; 
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	char str1[] = "abcabc";
 	char str2[] = "abcdefghijklmnopqrstuvwxyz";
 	
-	printf("Simple_AllCharsUnique(\"%s\") is %u.\n", str1, Simple_AllCharsUnique(str1));
+	printf("\nSimple_AllCharsUnique(\"%s\") is %u.\n", str1, Simple_AllCharsUnique(str1));
 	printf("Simple_AllCharsUnique(\"%s\") is %u\n", str2, Simple_AllCharsUnique(str2));
 	printf("Convoluted_AllCharsUnique(\"%s\") is %u.\n", str1, Convoluted_AllCharsUnique(str1));
 	printf("Convoluted_AllCharsUnique(\"%s\") is %u\n", str2, Convoluted_AllCharsUnique(str2));
@@ -173,7 +258,7 @@ int main(int argc, char *argv[])
 	char reverse1[] = "abc1234";
 	char reverse2[] = "abcdefghijklmnopqrstuvwxyz";
 	
-	printf("reverse(%s) is ", reverse1);
+	printf("\nreverse(%s) is ", reverse1);
 	reverse(reverse1);
 	printf("%s.\n", reverse1);
 	printf("reverse(%s) is ", reverse2);
@@ -185,11 +270,79 @@ int main(int argc, char *argv[])
 	char notPermutation1[] = "abc12";
 	char notPermutation2[] = "zyx890";
 	
-	printf("IsPermutation(%s, %s) is %u.\n", permutation1, permutation2, IsPermutation(permutation1, permutation2));
-	printf("IsPermutation(%s, %s) is %u.\n", permutation1, notPermutation1, IsPermutation(permutation1, notPermutation1));
-	printf("IsPermutation(%s, %s) is %u.\n", permutation1, notPermutation2, IsPermutation(permutation1, notPermutation2));
-	printf("IsPermutation(%s, %s) is %u.\n", permutation2, notPermutation1, IsPermutation(permutation2, notPermutation1));
-	printf("IsPermutation(%s, %s) is %u.\n", permutation2, notPermutation2, IsPermutation(permutation2, notPermutation2));
+	printf("\nIsPermutation(\"%s\", \"%s\") is %u.\n", permutation1, permutation2, IsPermutation(permutation1, permutation2));
+	printf("IsPermutation(\"%s\", \"%s\") is %u.\n", permutation1, notPermutation1, IsPermutation(permutation1, notPermutation1));
+	printf("IsPermutation(\"%s\", \"%s\") is %u.\n", permutation1, notPermutation2, IsPermutation(permutation1, notPermutation2));
+	printf("IsPermutation(\"%s\", \"%s\") is %u.\n", permutation2, notPermutation1, IsPermutation(permutation2, notPermutation1));
+	printf("IsPermutation(\"%s\", \"%s\") is %u.\n", permutation2, notPermutation2, IsPermutation(permutation2, notPermutation2));
+
+	char encode1[] = "Mr John Smith    ";
+	char encode2[] = "Mr John   Smith        ";
+	char encodeErrorTooLong[] = "Mr John   Smith         ";
+	char encodeErrorTooShort[] = "Mr John   Smith       ";
+	char beginsWithSpace[] = " Mr John   Smith          ";
+	char beginsWithSpaceTooShort[] = " Mr John   Smith         ";
+	
+	printf("\nEncodeSpaces(\"%s\", %u) = ", encode1, 13);
+	EncodeSpaces(encode1, 13);
+	printf("%s\n", encode1);
+	printf("EncodeSpaces(\"%s\", %u) = ", encode2, 15);
+	EncodeSpaces(encode2, 15);
+	printf("%s\n", encode2);
+	printf("EncodeSpaces(\"%s\", %u) = ", beginsWithSpace, 16);
+	EncodeSpaces(beginsWithSpace, 16);
+	printf("%s\n", beginsWithSpace);
+	
+	try
+	{
+		printf("EncodeSpaces(\"%s\", %u) throws ", encodeErrorTooLong, 15);
+		EncodeSpaces(encodeErrorTooLong, 15);
+	}
+	catch(char const*error)
+	{
+		if(strcmp("String contained too much trailing whitespace!", error) != 0)
+		{
+			printf("Wrong exception (\"%s\") thrown by EncodeSpaces(%s, 15)!\n", error, encodeErrorTooLong);
+		}
+		else
+		{
+			printf("\"%s\"\n", error);
+		}
+	}
+
+	try
+	{
+		printf("EncodeSpaces(\"%s\", %u) throws ", encodeErrorTooShort, 15);
+		EncodeSpaces(encodeErrorTooShort, 15);
+	}
+	catch(char const*error)
+	{
+		if(strcmp("Buffer too small to expand all spaces!", error) != 0)
+		{
+			printf("Wrong exception (\"%s\") thrown by EncodeSpaces(%s, 15)!\n", error, encodeErrorTooShort);
+		}
+		else
+		{
+			printf("\"%s\"\n", error);
+		}
+	}
+
+	try
+	{
+		printf("EncodeSpaces(\"%s\", %u) throws ", beginsWithSpaceTooShort, 16);
+		EncodeSpaces(beginsWithSpaceTooShort, 16);
+	}
+	catch(char const*error)
+	{
+		if(strcmp("Buffer too small to expand all spaces!", error) != 0)
+		{
+			printf("Wrong exception (\"%s\") thrown by EncodeSpaces(%s, 16)!\n", error, beginsWithSpaceTooShort);
+		}
+		else
+		{
+			printf("\"%s\"\n", error);
+		}
+	}
 	
 	return 0;
 }
