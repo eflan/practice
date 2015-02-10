37a139757496e537ba16ee95f2314517f179e9e2
Eliot Flannery <eliotf@gmail.com>
2015-02-10 14:25:53 -0800

    Print binary representation of double solution.

--- BitManipulation/eliot/BitManipulation.cpp
+++ BitManipulation/eliot/BitManipulation.cpp
@@ -63,6 +63,52 @@ void TestInsertBits(const unsigned int N, const unsigned int M, const unsigned i
 
 }
 
+void PrintBinary(double d)
+{
+	// binary representation is like so --
+	// 0th bit = 1/2, 1st bit = 1/4, 2nd bit = 1/8, etc.
+	if(d >= 1.0d || d <= 0.0d)
+	{
+		printf("%lf in binary is ERROR\n", d);
+	}
+	else
+	{
+		char binary[33];
+		size_t i = 0;
+		double temp = d;
+		
+		for(; i < 33; i++)
+		{
+			if(temp <= 0.0d)
+			{
+				break;
+			}
+
+			double bit = temp * 2.0d;
+			if(bit >= 1.0d)
+			{
+				binary[i] = '1';
+				temp = bit - 1.0d;
+			}
+			else
+			{
+				binary[i] = '0';
+				temp = bit;
+			}			
+		}
+		
+		if(temp > 0.0d)
+		{
+			printf("%lf in binary is ERROR\n", d);
+		}
+		else
+		{
+			binary[i] = '\0';
+			printf("%lf in binary is %s\n", d, binary);
+		}
+	}
+}
+
 bool mystery(int n)
 {
 	// tests if n is a power of 2
@@ -95,6 +141,11 @@ int main(int argc, char *argv[])
 		printf("(%u & %u) a.k.a. (%s & %s) == 0? %s.\n", i + 1, i, binaryN, binaryNMinus1, mystery(i + 1) ? ("Yes") : ("No"));
 	}
 	
+	printf("\n");
+	PrintBinary(0.72d);
+	PrintBinary(0.25d);
+	PrintBinary(0.125d);
+	PrintBinary(0.25d + 0.125d);
 	
 	return 0;
 }
