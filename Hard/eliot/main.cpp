6a65ebc96a44fcf13b54557588a1796e004927b9
Eliot Flannery <eliotf@gmail.com>
2015-03-10 18:27:41 -0700

    Perfect shuffle and choose M from N solutions.

--- Hard/eliot/main.cpp
+++ Hard/eliot/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
+#include <vector>
 
 int add(int a, int b)
 {
@@ -35,6 +37,51 @@ int add(int a, int b)
 	return sum;
 }
 
+const size_t perfectRand()
+{
+	return rand();
+}
+
+typedef unsigned char Card;
+#define INVALID_CARD 0
+
+void perfectShuffle(Card cards[52])
+{
+	Card shuffled[52] = {};
+	
+	for(Card c = 1; c <= 52; c++)
+	{
+		size_t index = perfectRand() % 52;
+	
+		while(shuffled[index] != INVALID_CARD)
+		{
+			index = perfectRand() % 52;
+		}
+		
+		shuffled[index] = c;
+	}
+	
+	memcpy(cards, shuffled, sizeof(shuffled));
+}
+
+std::vector<int> choose(const size_t m, const std::vector<int> &numbers)
+{
+	auto it = numbers.begin();
+	std::advance(it, m);
+	std::vector<int> subset(numbers.begin(), it);
+	
+	for(size_t i = m; i < numbers.size(); i++)
+	{
+		size_t k = perfectRand() % i;
+		if(k < m)
+		{
+			subset[k] = numbers[i];
+		}
+	}
+	
+	return subset;
+}
+
 int main(int argc, char *argv[])
 {
 	srand(time(NULL));
@@ -56,6 +103,34 @@ int main(int argc, char *argv[])
 		}
 		printf("%d + %d = %d (should be %d)\n", a, b, add(a, b), (a + b));
 	}
+	
+	Card cards[52];
+	for(Card c = 1; c <= 52; c++)
+	{
+		cards[c - 1] = c;
+	}
+	perfectShuffle(cards);
+	printf("\nPerfect shuffle = ");
+	for(size_t i = 0; i < 52; i++)
+	{
+		printf("%u, ", cards[i]);
+	}
+	printf("\n");
+	
+	std::vector<int> set(100);
+	for(size_t i = 0; i < 100; i++)
+	{
+		set[i] = i;
+	}
+	
+	std::vector<int> subset = choose(10, set);
+	
+	printf("\nChose {");
+	for(size_t i = 0; i < subset.size(); i++)
+	{
+		printf("%d, ", subset[i]);
+	}
+	printf("}\n");
 	
 	return 0;
 }
